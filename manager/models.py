e2128e9db2aba81667f6580bfc3b6ceef55e089c
Empty <make772@ukr.net>
2025-03-19 20:26:02 +0200

    add to manager/models.py model Task

--- manager/models.py
+++ manager/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
-# Create your models here.
+
+class Task(models.Model):
+    PRIORITY_CHOICES = [
+        ("Urgent", "Urgent"),
+        ("High", "High"),
+        ("Middle", "Middle"),
+        ("Low", "Low"),
+    ]
+
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    deadline = models.DateField()
+    is_completed = models.BooleanField(default=False)
+    priority = models.CharField(
+        max_length=7,
+        choices=PRIORITY_CHOICES,
+        default="Middle"
+    )
+    task_type = models.ForeignKey("TaskType", on_delete=models.PROTECT)
+    assignees = models.ManyToManyField("Worker", related_name="tasks_assigned")
+    project = models.ForeignKey(
+        "Project", on_delete=models.PROTECT,
+        related_name="tasks_project"
+    )
+    team = models.ManyToManyField("Team", related_name="tasks_team")
