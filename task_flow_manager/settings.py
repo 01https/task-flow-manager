70c3141a9db8fc9765a0d3817c79c689fe1102b0
Empty <make772@ukr.net>
2025-03-21 01:26:39 +0200

    add in settings.py LOGIN REDIRECT & LOGOUT REDIRECT

--- task_flow_manager/settings.py
+++ task_flow_manager/settings.py
@@ -126,6 +126,10 @@ STATICFILES_DIRS = [
 
 ASSETS_ROOT = '/static/assets'
 
+LOGIN_REDIRECT_URL = "/"
+
+LOGOUT_REDIRECT_URL = "login"
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
